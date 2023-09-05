1183c4b6b651d6d41b82b89710ec7a225ba5891c
houbowii <houbowii@sina.com>
2023-09-05 23:56:34 +0800

    github start!

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # blurdep-cli
 
-a function show u clearly dependencies struct
+a npm tool show u clearly dependencies struct
 
-# Dependencies
-you need these platform to use the code
- - Chrome broswer
+# ENV
+I don't ensure that whether platform differs make effects(if you find something uncomfortable pls tell mi :)
 
- - nodejs 8.0^
+- Chrome stable/lastest
+
+# Usage
+
+Step 1: Install the package global
+```shell
+npm install -g blurdep-cli
+```
+
+Step 2: Use it!
+```shell
+blurdep --root=[pathOfyourASMproj] --depth=[number(optional)] --port=[number"(3000, 65535]"(optional)]
+```
--- bin/index.js
+++ bin/index.js
@@ -8,9 +8,16 @@ let thisfilelocation = path.resolve(__dirname, "..");
 let jsondatalocation = path.join(thisfilelocation, "./client/data.json")
 
 let depthFlagIndex = (process.env.npm_config_depth) ? process.env.npm_config_depth : 2;
-let root = (process.env.npm_config_root) ? process.env.npm_config_root : "";
 
-handle.generateDependencyTree(root, "", "depDependencies", depthFlagIndex)
+let root = process.argv.find(arg => arg.startsWith('--root='));
+let rootpath;
+if (root) {
+  rootpath = root.split('=')[1];
+} else {
+  console.log('root parameter not provided');
+}
+
+handle.generateDependencyTree(rootpath, "", "depDependencies", depthFlagIndex)
   .then((depTree) => {
     bestFS.writeFile(jsondatalocation, JSON.stringify(depTree));
   })
