beb486bd878189d75e8b3322b6107a9c78e133e3
houbowii <houbowii@sina.com>
2023-09-05 23:11:55 +0800

    beta1.0 officially arrived

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
+
+const path = require("path");
 const handle = require("./server/handle");
 const bestFS = require("./server/bestFS");
 
-handle.generateDependencyTree("", "depDependencies", 2)
+let thisfilelocation = path.resolve(__dirname, "..");
+let jsondatalocation = path.join(thisfilelocation, "./client/data.json")
+
+let depthFlagIndex = (process.env.npm_config_depth) ? process.env.npm_config_depth : 2;
+let root = (process.env.npm_config_root) ? process.env.npm_config_root : "";
+
+handle.generateDependencyTree(root, "", "depDependencies", depthFlagIndex)
   .then((depTree) => {
-    bestFS.writeFile("./client/data.json", JSON.stringify(depTree));
-    }) 
+    bestFS.writeFile(jsondatalocation, JSON.stringify(depTree));
+  })
   .catch((error) => console.error(error));
--- server/handle.js
+++ server/handle.js
@@ -2,15 +2,25 @@ const path = require("path");
 const bestFS = require("./bestFS");
 const DepTree = require("./depTree");
 
-async function generateDependencyTree(path2resolve = "", field = "dependencies", depth = 1) {
-    let target = (path2resolve === "") ? path.resolve("./package.json") : path.resolve("./node_modules/", path2resolve, "./package.json");
+async function generateDependencyTree(root, path2resolve = "", field = "dependencies", depth = 1) {
+        // let target = (path2resolve === "") ? return : path2resolve;
+    if (root==="") return;
+    let target;
+    if (path2resolve===""){
+        target = path.resolve(root, "./package.json");
+    } else {
+        target = path.resolve(root, "./node_modules", path2resolve, "./package.json")
+    }
+    if (target==="") {
+        return;
+    }
     try {
         let content = await bestFS.readFile(target);
         let { dependencies = {} } = JSON.parse(content);
         let depTree = Object.entries(dependencies || {}).map(([key, value]) => new DepTree(key, value));
         if (depth > 0) {
             for (let i = 0; i < depTree.length; i++) {
-                let childTree = await generateDependencyTree(depTree[i].name, field, depth - 1);
+                let childTree = await generateDependencyTree(root, depTree[i].name, field, depth - 1);
                 childTree.forEach(element => {
                     depTree[i].addChildTree(element);
                 });
